3e61fa54613b5059efe90ce762e95e1713be0270
SummyChou <summychou@gmail.com>
2016-05-27 03:54:31 +0800

    summychou commit

---
+++ insertdata.sql
@@ -0,0 +1,7 @@
+USE AliyunMusic;
+LOAD DATA INFILE '/home/ftp/myblog/mars_tianchi_songs.csv' INTO TABLE mars_tianchi_songs
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"';
+LOAD DATA INFILE '/home/ftp/myblog/mars_tianchi_user_actions.csv' INTO TABLE mars_tianchi_user_actions
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"';
